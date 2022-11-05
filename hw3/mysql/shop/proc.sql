e351c84d62fafd5543b4ea411807839a15964757
imishustina <mishusitina12@inbox.ru>
2022-11-05 14:13:53 +0400

    transactions

---
+++ hw3/mysql/shop/proc.sql
@@ -0,0 +1,11 @@
+create procedure `transactions`()
+    DETERMINISTIC
+    SQL SECURITY INVOKER
+    COMMENT 'delete all products that no longer in access, change Mail for VK, TVs for sale'
+BEGIN
+start transaction;
+delete from product where quantity = 0;
+update producer set producer_name = 'VK' where producer_name = 'Mail';
+update product set price = price - 10.00 where product_name = 'TV';
+commit;
+end;
